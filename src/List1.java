458a134ac0df2dad397b3071920a08e54be5bd9b
ahsishrasal17 <119799103+ahsishrasal17@users.noreply.github.com>
2026-01-10 16:04:47 +0530

    Remove method implementation in List

--- src/List1.java
+++ src/List1.java
@@ -37,5 +37,14 @@ public class List1 {
         for(String n : list){
             System.out.println(n + " ");
         }
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.remove(1);
+        System.out.println(list1);
+        list1.remove(Integer.valueOf(1));
+        System.out.println(list1);
     }
 }
